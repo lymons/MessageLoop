6aa14c430e67cd7c457742b9863b1a032c093355
lymons <lymons@gmail.com>
2014-03-20 09:28:20 +0800

    Initial Commit

---
+++ MessageLoop/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MessageLoop
+//
+//  Created by Lymons on 14-3-20.
+//  Copyright (c) 2014年 Lymons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
